cc5e9d9db27bafdaab0fab86b3ee5c0b3b3df27b
JeongMin-98 <jeongmin981@gamil.com>
2020-08-30 18:41:45 +0900

    countingsort.py
    countionsort.py

---
+++ .vscode/countingsort.py
@@ -0,0 +1,17 @@
+def CountingSort(A,B,k):
+    C = [0]*k
+
+    for i in range(0,len(B)):
+        C[A[i]]+=1
+
+    for i in range(1,len(C)):
+        C[i] += C[i-1]
+
+    for i in range(len(B)-1,-1,-1):
+        B[C[A[i]]-1] = A[i]
+        C[A[i]] -=1
+
+a = [0,4,1,3,1,2,4,1]
+b = [0]*len(a)
+CountingSort(a,b,5)
+print(b)
