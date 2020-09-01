60a9c008421d8a9c09d28636dfc14d817fcdb1ab
JeongMin-98 <jeongmin981@gamil.com>
2020-09-01 13:02:17 +0900

    Update minmax.py
    [SW Expert Academy] source code

--- .vscode/minmax.py
+++ .vscode/minmax.py
@@ -1,28 +1,10 @@
 T = int(input()) # test case
-while T > 0:
-    
-    N = int(input()) #case마다 입력되는 갯수
-    N = [0]*N
+for i in range(1,T+1):
+    K = int(input())
+    N = [0]*K
     # 정수값 입력
-    for i in range(len(N)):
-        N[i] = int(input())
-    
+    a = map(int, input().split())
+    N = list(a)    
     # 정수 리스트 중 최솟값 구하기
-    min = N[0]
-    for i in range(len(N)):
-        if min<N[i]:
-            min = min
-        else:
-            min = N[i]
-    
-    # 정수 리스트 중 최댓값 구하기
-    max = N[0]
-    for i in range(len(N)):
-        if max>N[i]:
-            max = max
-        else:
-            max = N[i]
-    result = max - min
-    print(result)
-    T -=1
-
+    result = max(N)-min(N)
+    print('#{0} {1}'.format(i, result))
