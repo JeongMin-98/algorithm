f42d63a56667779aeee549c93bdb205891f731a6
JeongMin-98 <jeongmin981@gamil.com>
2020-09-08 02:43:34 +0900

    Update [4839]이진탐색.py
    Update [4839]이진탐색.py

---
+++ .vscode/list2/[4839]이진탐색.py
@@ -0,0 +1,29 @@
+def binarySearch(a, key):
+    start = 0
+    end = len(a)-1
+    count = 0
+    find = False
+    while not find:
+        middle = start + (end - start) // 2
+        if key == a[middle]:
+            find = True
+        elif key < a[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+        count += 1
+    return count
+
+T = int(input())
+
+for test_case in range(1,T+1): # test_case
+    P, A, B = map(int, input().split()) # P : 전체 쪽 수, A : A가 찾아야하는 쪽, B : B가 찾아야하는 쪽
+    book = list(range(1,P+1))
+    FindA = binarySearch(book, A)
+    FindB = binarySearch(book, B)
+    if FindA < FindB:
+        print("#{0} A".format(test_case))
+    elif FindA > FindB:
+        print("#{0} B".format(test_case))
+    else:
+        print("#{0} 0".format(test_case))
