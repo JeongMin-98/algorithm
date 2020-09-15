d1922fa16f0f0a8155366de0948292130ab66215
JeongMin-98 <jeongmin981@gmail.com>
2020-09-15 12:02:29 +0900

    update [4836] 문자열 비교.py
    update file
    runtime error

--- .vscode/string/[4836] 문자열비교.py
+++ .vscode/string/[4836] 문자열비교.py
@@ -1,4 +1,40 @@
+def findstring(p, t):
+    i = 0
+    j = 0
+    M = len(p) # M : pattern의 길이
+    N = len(t) # N : 전체 string의 길이
+    i = i + M - 1
+    j = j + M - 1 # i : pattern index, j = str(t) index
+    pp = 0
+    count = 0
+    skip = list(reversed(p))
+    while j<N:
+        if p[i]!=t[j]:
+            if t[j] in skip:
+                for x in range(M):
+                    if t[j] == skip[x]:
+                        j += x
+            else:
+                j += M
+        else:
+            for x in range(M):
+                if t[j] == skip[x]:
+                    j -= 1
+                    pp += 1
+                else:
+                    break
+            if pp == M:
+                count+=1
+            j = j + pp + M
+        
+    return count
+
 T = int(input())
 
 for test_case in range(1,T+1):
-    
+
+    pattern = input()
+    total = input()
+
+    count = findstring(pattern, total)
+    print("#{0} {1}".format(test_case, count))
